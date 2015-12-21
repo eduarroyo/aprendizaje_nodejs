b8a22d109768003b66e764e70ece3cbb56e30bc6
= <=>
2015-12-21 12:42:41 +0100

    Ejercicio 4 learnyoumongo, corrección ejercicio 3 learnyoumongo

--- learnyoumongo/04_FindProject/index.js
+++ learnyoumongo/04_FindProject/index.js
@@ -1,16 +1,17 @@
 /*
  Learn MongoDB
 ───────────────
- FIND
- Exercise 3 of 9
+ FIND PROJECT
+ Exercise 4 of 9
 
-Here we will learn how to search for documents.
-
-For all of the exercises, the database is learnyoumongo.
-So, the url would be something like: mongodb://localhost:27017/learnyoumongo
+Here we will learn how to search for documents but only fetch the fields
+we need. Also known as projection in MongoDB
 
 Use the parrots collection to find all documents where age
 is greater than the first argument passed to your script.
+
+The difference from the last lesson will be that we only want the
+name and age properties
 
 Using console.log, print the documents to stdout.
 
@@ -18,37 +19,28 @@ Using console.log, print the documents to stdout.
 
 ## HINTS
 
-To connect to the database, one can use something like this:
-
-    var mongo = require('mongodb').MongoClient
-    mongo.connect(url, function(err, db) {
-      // db gives access to the database
-    })
-
-To get a collection, one can use db.collection('<collection name>').
-
 To find a document or documents, one needs to call find() on the collection.
 
 Find is a little bit different than what we are used to seeing.
 
-Keep in mind, process.argv is an array of strings. To convert to an integer, you could use parseInt()
-
 Here is an example:
 
     collection.find({
-      name: 'foo'
+        name: 'foo'
+    }, {
+        name: 1,
+        age: 1,
+        _id: 0
     }).toArray(function(err, documents) {
-        //
-    });
+    })
 
 If your program does not finish executing, you may have forgotten to
 close the db. That can be done by calling db.close() after you
 have finished.
 
-## Resources:
+## Resource:
 
-  * http://docs.mongodb.org/manual/reference/method/db.collection.find/
-  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/parseInt
+  * http://docs.mongodb.org/manual/reference/method/db.collection.find/#explicitly-exclude-the-id-field
 */
 
 var mongo = require('mongodb').MongoClient;
@@ -64,12 +56,11 @@ mongo.connect('mongodb://localhost:27017/learnyoumongo', function (err, db) {
     } else {
         collection = db.collection('parrots');
         collection.find(
-            // Este objeto es la configuración de filtrado.
-            // la propiedad indica el campo de filtrado y el valor de la propiedad es el filtro
-            // para cada propiedad, 
+            // Proyección: registrso con "age" mayor ($gt) que ageFilter.
             {age: {$gt: ageFilter}},
-            // En este ejercicio no es necesario especificar la selección de campos
-            //{name: 1, age: 1}
+            // Selección: poner a 1 los campos incluidos y a 0 los no incluidos.
+            // Parece que hay que poner explícitamente los campos que no queremos seleccionar
+            {name: 1, age: 1, _id: 0}
         ).toArray(function (err, parrots) {
             // if(err) throw err (de la solución)
             if (err) {
@@ -80,5 +71,4 @@ mongo.connect('mongodb://localhost:27017/learnyoumongo', function (err, db) {
             db.close();
         });
     }
-
 });
